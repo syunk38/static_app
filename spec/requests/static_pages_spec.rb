26aba504c20652c3ac3b6e24a327adbda4f0119f
syunk38 <syunk38@gmail.com>
2015-05-11 00:16:01 +0900

    save static app

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
+  let(:base_title) {"Ruby on Rails Tutorial Static App |"}
 
+  describe "Home page" do
+    let(:title) {"Home"}
     it "'Static App'が表示されているかを確認するテスト" do
       visit '/static_pages/home'
       expect(page).to have_content('Static App')
@@ -16,22 +18,46 @@ describe "Static pages" do
 
     it "正しいタイトルを表示しているか確認するテスト" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Static App | Home")
+      expect(page).to have_title("#{base_title} #{title}")
     end
   end
 
   describe "Help page" do
-
+    let(:title) {"Help"}
     it "'Help'が表示されているかを確認するテスト" do
       visit '/static_pages/help'
-      expect(page).to have_content('Help')
+      expect(page).to have_content("#{title}")
+    end
+
+    it "正しいタイトルを表示しているか確認するテスト" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} #{title}")
     end
   end
 
   describe "About page" do
+    let(:title) {"About us"}
     it "'About us'が表示されているかを確認するテスト"do
       visit '/static_pages/about'
-      expect(page).to have_content('About us')
+      expect(page).to have_content("#{title}")
+    end
+
+    it "正しいタイトルを表示しているか確認するテスト" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} #{title}")
+    end
+  end
+
+  describe "Contact page" do
+    let(:title) {"Contact"}
+    it "Contactが表示されているかを確認するテスト"do
+    visit '/static_pages/contact'
+    expect(page).to have_content("#{title}")
+    end
+
+    it "正しいタイトルを表示しているか確認するテスト" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} #{title}")
     end
   end
 
